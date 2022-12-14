63f299f76ee07adae562cd5a161a435ad5df6144
Minhye Kang <alsgp9898@naver.com>
2022-12-14 23:42:36 +0900

    feat: 사용자 숫자 입력 유효성 검사 코드 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
+const Validation = require("./Validation");
 const INPUT_QUERY = Object.freeze({
   ask_user_number: "숫자를 입력해주세요 : ",
   ask_restart_or_done: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
@@ -7,6 +8,7 @@ const INPUT_QUERY = Object.freeze({
 const InputView = {
   readUserNumber(readLineCallback) {
     Console.readLine(INPUT_QUERY.ask_user_number, (number) => {
+      Validation.checkUserNumber(number);
       readLineCallback(number);
     });
   },
---
+++ src/Validation.js
@@ -0,0 +1,18 @@
+const PREFIX = '[ERROR]'
+const ERROR_MESSAGE = Object.freeze({
+  not_number: `${PREFIX} 숫자를 입력해야 합니다.`,
+  not_three_digits: `${PREFIX} 3자리 숫자를 입력해야 합니다.`,
+  duplicate: `${PREFIX} 중복되지 않는 3자리 숫자를 입력해야 합니다.`,
+  out_of_range: `${PREFIX} 1부터 9까지의 숫자를 입력해야 합니다.`,
+})
+
+const Validation = {
+  checkUserNumber(number) {
+    if (isNaN(number)) throw new Error(ERROR_MESSAGE.not_number);
+    if (number.length !== 3) throw new Error(ERROR_MESSAGE.not_three_digits);
+    if (new Set([...number]).size !== 3) throw new Error(ERROR_MESSAGE.duplicate);
+    if (number.includes('0')) throw new Error(ERROR_MESSAGE.out_of_range);
+  }
+}
+
+module.exports = Validation;
