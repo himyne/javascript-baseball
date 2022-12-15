bc5b942e40569b506042dacfa735e9b238b8cfce
Minhye Kang <alsgp9898@naver.com>
2022-12-15 14:46:15 +0900

    feat: 자동차 이름 입력 유효성 검사 추가

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,13 @@
 const INPUT_QUERY = {
   car_name: '자동차 이름을 5자 이하 콤마로 구분하여 입력해주세요.\n'
-}
+};
 
-module.exports = {INPUT_QUERY}
+const ERROR = {
+  not_five_digits: '자동차의 이름은 5자 이하여야 합니다.',
+  blank: '아무것도 입력하지 않았습니다.',
+  not_name: '이름이 없는 자동차가 있습니다.',
+  not_competitor: '자동차 개수는 한대 이상이어야 합니다.',
+  duplicate: '중복된 자동차가 있습니다.'
+};
+
+module.exports = { INPUT_QUERY, ERROR };
---
+++ src/Validation.js
@@ -0,0 +1,14 @@
+const { ERROR } = require('./Constants');
+
+const Validation = {
+  checkCarName(cars) {
+    const carArray = cars.split(',').map((car) => car.trim());
+    if (!cars || !cars.trim()) throw new Error(ERROR.blank);
+    if (carArray.some((car) => car.length > 5)) throw new Error(ERROR.not_five_digits);
+    if (carArray.some((car) => !car)) throw new Error(ERROR.not_name);
+    if (new Set([...carArray]).size !== carArray.length) throw new Error('중복');
+    return carArray;
+  }
+};
+
+module.exports = Validation;
