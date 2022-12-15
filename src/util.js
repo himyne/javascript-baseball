bc56330d4e076eb1f971107dc534d9087e15c86f
Minhye Kang <alsgp9898@naver.com>
2022-12-15 15:07:48 +0900

    feat: 0에서 9까지의 랜덤한 숫자를 만드는 함수 생성

--- src/util.js
+++ src/util.js
@@ -1,12 +1,19 @@
-const {Console} = require('@woowacourse/mission-utils');
-
-const catchError = (input, validation)  => {
+const { Console, Random } = require('@woowacourse/mission-utils');
+const RANGE_NUMBER = {
+  start_inclusive: 0,
+  end_inclusive: 9
+};
+const catchError = (input, validation) => {
   try {
     return validation(input);
   } catch (error) {
     Console.print(error);
     return false;
   }
-}
+};
 
-module.exports = catchError;
+const makeRandomNumber = () => {
+  return Random.pickNumberInRange(RANGE_NUMBER.start_inclusive, RANGE_NUMBER.end_inclusive);
+};
+
+module.exports = {catchError, makeRandomNumber};
