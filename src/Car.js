d2ad7cbb8c4c55b1e8181c1b251eb6bab080279c
Minhye Kang <alsgp9898@naver.com>
2022-12-15 15:34:36 +0900

    feat: 4이상인 자동차만 전진하는 기능 추가

--- src/Car.js
+++ src/Car.js
@@ -3,15 +3,18 @@ const { makeRandomNumber } = require('./util');
 
 class Car {
   #cars;
+  #carRanking;
 
   constructor() {
     this.#cars = {}; 
+    this.#carRanking = {};
   }
 
   saveCarInfo() {
     InputView.readCarName((carArray) => {
       carArray.forEach((car) => {
         this.#cars[car] = 0
+        this.#carRanking[car] = 0
       })
       this.saveAttemptNumber();
     });
@@ -19,7 +22,9 @@ class Car {
 
   saveAttemptNumber() {
     InputView.readAttemptNumber((number) => {
-      this.giveRandomNumberToCar();
+      for(let i=0; i<number; i++){
+        this.giveRandomNumberToCar();
+      }
     });
   }
 
@@ -27,6 +32,11 @@ class Car {
     Object.keys(this.#cars).forEach((key) => {
       this.#cars[key] = makeRandomNumber();
     })
+    for(let key in this.#cars){
+      if(this.#cars[key] >= 4){
+        this.#carRanking[key]++;
+      }
+    }
   }
 }
 
