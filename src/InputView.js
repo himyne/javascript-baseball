8732bd6678c7d8fd6b1b31b1e478d127ed350274
Minhye Kang <alsgp9898@naver.com>
2022-12-15 14:49:35 +0900

    feat: 입력이 잘못되면 다시 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,10 +1,14 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { INPUT_QUERY } = require('./Constants');
+const catchError = require('./util');
+const { checkCarName } = require('./Validation');
 
 const InputView = {
   readCarName(readCarNameCallback) {
     Console.readLine(INPUT_QUERY.car_name, (cars) => {
-      readCarNameCallback(cars)
+      cars = catchError(cars, checkCarName);
+      if(!cars) return this.readCarName(readCarNameCallback);
+      readCarNameCallback(cars);
     });
   }
 };
--- src/Validation.js
+++ src/Validation.js
@@ -6,7 +6,7 @@ const Validation = {
     if (!cars || !cars.trim()) throw new Error(ERROR.blank);
     if (carArray.some((car) => car.length > 5)) throw new Error(ERROR.not_five_digits);
     if (carArray.some((car) => !car)) throw new Error(ERROR.not_name);
-    if (new Set([...carArray]).size !== carArray.length) throw new Error('중복');
+    if (new Set([...carArray]).size !== carArray.length) throw new Error(ERROR.duplicate);
     return carArray;
   }
 };
